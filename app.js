f8ad56dac8e463c37fa311b695a786cbc2267769
kabindrashrsth <44717552+kabindrashrsth@users.noreply.github.com>
2019-06-07 19:35:29 -0500

    Updated
    The transposer now accepts minor, seventh, and ninth chords

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let noteArr = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B"];
+// let noteArr = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B"];
+let noteArr1 = ["C", "D", "E", "F", "G", "A", "B"];
+let noteArr2 = ["m", "b", "#", "7", "3", "9"];
+let noteArr3 = ["m", "7", "3", "9"];
+let noteArr4 = ["7", "3", "9"];
 
 let inputLength = 7;
 
@@ -12,71 +16,168 @@ function transposeProcess(e) {
 
   let valid = true; // checks if user-input is valid or not
 
+  document.getElementById("new-key").style.border = "none";
+  document.getElementById("user-input1").style.border = "none";
+
+  let prevKID = document.getElementById("user-input1");
+  let newKID = document.getElementById("new-key");
+
   // Previous Key
-  let prevK = noteArr.indexOf(
-    document.getElementById("user-input1").value.toUpperCase()
-  );
+  let prevK = noteArr1.indexOf(capitalizedFirstLetter(prevKID.value).charAt(0));
 
   // New Key
-  let newK = noteArr.indexOf(
-    document.getElementById("new-key").value.toUpperCase()
-  );
+  let newK = noteArr1.indexOf(capitalizedFirstLetter(newKID.value).charAt(0));
 
-  let addingFactor = 12 + (newK - prevK);
+  console.log(prevK);
+  console.log(newK);
 
-  // Array that stores user input
-  let inputArr = [];
+  if (newK != -1 && prevK != -1) {
+    // newK is valid, so sets green border
+    document.getElementById("new-key").style.border = "green solid 1px";
 
-  let outputVal = [];
+    let addingFactor = noteArr1.length + (newK - prevK);
 
-  for (let j = 1; j <= inputLength && valid; j++) {
-    let inputVal = document.getElementById(`user-input${j}`).value;
+    // Array that stores user input
+    let inputArr = [];
 
-    if (inputVal != "" && noteArr.indexOf(inputVal.toUpperCase()) === -1) {
-      valid = false;
-    } else if (inputVal != "") {
-      inputArr.push(inputVal.toUpperCase());
-      // transpoing and adding the values to the array outputVal
-      let i = noteArr.indexOf(inputVal.toUpperCase());
-      outputVal.push(noteArr[(i + addingFactor) % noteArr.length]);
+    let outputVal = [];
+
+    for (let j = 1; j <= inputLength && valid; j++) {
+      let inputID = document.getElementById(`user-input${j}`);
+      let inputVal = inputID.value;
+
+      if (inputVal != "") {
+        inputVal = inputVal.trim();
+        inputVal = capitalizedFirstLetter(inputVal);
+
+        if (!validateInput(inputVal)) {
+          valid = false;
+
+          inputID.style.border = "none";
+          inputID.style.border = "red solid 2px";
+        } else {
+          inputID.style.border = "none";
+          inputID.style.border = "green solid 1px";
+          inputArr.push(inputVal);
+          // transpoing and adding the values to the array outputVal
+          let i = noteArr1.indexOf(inputVal.charAt(0));
+          outputVal.push(
+            noteArr1[(i + addingFactor) % noteArr1.length] + inputVal.slice(1)
+          );
+        }
+      }
     }
-  }
 
-  if (valid) {
-    // For output
-    let nOutput = document.getElementById("newOutput");
-    let oOutput = document.getElementById("oldOutput");
+    if (valid) {
+      // For output
+      let nOutput = document.getElementById("newOutput");
+      let oOutput = document.getElementById("oldOutput");
 
-    let newOutputString = "";
-    let oldOldputString = "";
+      let newOutputString = "";
+      let oldOldputString = "";
 
-    for (let i = 0; i < inputArr.length; i++) {
-      oldOldputString += inputArr[i] + "   ";
-      newOutputString += outputVal[i] + "   ";
+      for (let i = 0; i < inputArr.length; i++) {
+        oldOldputString += inputArr[i] + "   ";
+        newOutputString += outputVal[i] + "   ";
+      }
+
+      oOutput.innerText = oldOldputString;
+
+      nOutput.innerText = newOutputString;
+
+      document.getElementById("oldOutputCard").style.visibility = "visible";
+      document.getElementById("newOutputCard").style.visibility = "visible";
+    } else {
+      showWarning();
     }
-
-    oOutput.innerText = oldOldputString;
-
-    nOutput.innerText = newOutputString;
-
-    document.getElementById("oldOutputCard").style.visibility = "visible";
-    document.getElementById("newOutputCard").style.visibility = "visible";
   } else {
-    document.getElementById("warning").style.visibility = "visible";
-
-    document.getElementById("oldOutputCard").style.visibility = "hidden";
-    document.getElementById("newOutputCard").style.visibility = "hidden";
+    if (newK === -1) {
+      document.getElementById("new-key").style.border = "red solid 2px";
+      showWarning();
+    }
+    if (prevK === -1) {
+      document.getElementById("user-input1").style.border = "red solid 2px";
+      showWarning();
+    }
   }
+}
+
+// pre: accepts a string as parameter
+// post: returns a string with first letter in upper-case
+function capitalizedFirstLetter(inputVal) {
+  return inputVal.charAt(0).toUpperCase() + inputVal.slice(1);
+}
+
+// displays warning
+function showWarning() {
+  document.getElementById("warning").style.visibility = "visible";
+
+  document.getElementById("oldOutputCard").style.visibility = "hidden";
+  document.getElementById("newOutputCard").style.visibility = "hidden";
+}
+
+// validates the user input
+function validateInput(input) {
+  let isValid = false;
+
+  switch (input.length) {
+    case 1:
+      if (noteArr1.indexOf(input) != -1) {
+        isValid = true;
+      }
+      break;
+
+    case 2:
+      if (
+        noteArr1.indexOf(input.charAt(0)) != -1 &&
+        noteArr2.indexOf(input.charAt(1)) != -1
+      ) {
+        isValid = true;
+      }
+      break;
+
+    case 3:
+      if (
+        noteArr1.indexOf(input.charAt(0)) != -1 &&
+        noteArr2.indexOf(input.charAt(1)) != -1 &&
+        noteArr3.indexOf(input.charAt(2)) != -1 &&
+        input.charAt(1) != input.charAt(2)
+      ) {
+        isValid = true;
+      }
+      break;
+
+    case 4:
+      if (
+        noteArr1.indexOf(input.charAt(0)) != -1 &&
+        noteArr2.indexOf(input.charAt(1)) != -1 &&
+        noteArr3.indexOf(input.charAt(2)) != -1 &&
+        noteArr4.indexOf(input.charAt(3)) != -1 &&
+        input.charAt(1) != input.charAt(2) &&
+        input.charAt(2) != input.charAt(3)
+      ) {
+        isValid = true;
+      }
+      break;
+  }
+
+  return isValid;
 }
 
 /*
 //Things to fix:
 1. 
 2. UI - DONE for now
-3. What to do when user enters a minor chord
+3. What to do when user enters a minor chord?
+   Make two arrays: one for first letter and another for all possible second characters. - DONE
+
 4. 
 5. undefined - DONE
 6. What if user enters wrong letters? - DONE
-7. IDK why there is a scroll bar
+7. IDK why there is a scroll bar - DONE
 8. make it reactive
+9. not accepting flats. - DONE
+10. how did Bb -> A gave G#? -DONE
+11. how to deal with 'add's?
+
 */
