be96607a67a5da5d4f3852b5c5c76ae38164ca4c
kabindrashrsth <44717552+kabindrashrsth@users.noreply.github.com>
2019-07-17 16:07:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,111 +1,65 @@
+// list of valid input values
 let noteArr1 = [
   "C",
   "C#",
   "D",
-  "Eb",
+  "D#",
   "E",
   "F",
   "F#",
   "G",
   "G#",
   "A",
-  "Bb",
+  "A#",
   "B"
 ];
-//let noteArr2 = ["m", "b", "#", "7", "3", "9"];
 let noteArr2 = ["m"];
 let noteArr3 = ["7", "9", "2", "4"];
 
+// number of input boxes
 let inputLength = 12;
 
 let transposeBtn = document.getElementById("transpose-btn");
-
 transposeBtn.addEventListener("click", transposeProcess);
 
+// transpose process
 function transposeProcess(e) {
   e.preventDefault();
-
-  let valid = true; // checks if user-input is valid or not
-
-  document.getElementById("new-key").style.border = "none";
-  document.getElementById("old-key").style.border = "none";
 
   let prevKID = document.getElementById("old-key");
   let newKID = document.getElementById("new-key");
 
-  // Previous Key
-  let prevK = noteArr1.indexOf(
-    capitalizedFirstLetter(filterInput(prevKID.value))
-  );
+  let isNewKValid = validateInput(capitalizedFirstLetter(newKID.value));
+  let isPrevKValid = validateInput(capitalizedFirstLetter(prevKID.value));
+  // if new key and previous key are valid
+  if (isNewKValid && isPrevKValid) {
+    // indexOfNewK is valid, so sets oragne border
+    prevKID.style.border = "#E88F00 solid 2px";
+    newKID.style.border = "#E88F00 solid 2px";
 
-  // New Key
-  let newK = noteArr1.indexOf(
-    capitalizedFirstLetter(filterInput(newKID.value))
-  );
+    // Index of Previous Key
+    let indexOfPrevK = noteArr1.indexOf(
+      capitalizedFirstLetter(filterInput(prevKID.value))
+    );
 
-  if (newK != -1 && prevK != -1) {
-    // newK is valid, so sets green border
-    document.getElementById("old-key").style.border = "#E88F00 solid 2px";
-    document.getElementById("new-key").style.border = "#E88F00 solid 2px";
+    // Index of New Key
+    let indexOfNewK = noteArr1.indexOf(
+      capitalizedFirstLetter(filterInput(newKID.value))
+    );
 
-    let addingFactor = noteArr1.length + (newK - prevK);
+    let addingFactor = noteArr1.length + (indexOfNewK - indexOfPrevK);
 
     // Array that stores user input
     let inputArr = [];
+    let outputArr = [];
 
-    let outputVal = [];
+    // Checks each input node and processes the input values
+    processInput(inputArr, outputArr, addingFactor);
 
-    for (let j = 1; j <= inputLength && valid; j++) {
-      let inputID = document.getElementById(`user-input${j}`);
-      let inputVal = inputID.value;
-
-      if (inputVal != "") {
-        inputVal = inputVal.trim();
-        inputVal = capitalizedFirstLetter(inputVal);
-
-        if (!validateInput(inputVal)) {
-          valid = false;
-
-          inputID.style.border = "none";
-          inputID.style.border = "red solid 2px";
-        } else {
-          inputID.style.border = "none";
-          inputID.style.border = "green solid 1px";
-          inputArr.push(inputVal);
-          // transpoing and adding the values to the array outputVal
-          let i = noteArr1.indexOf(filterInput(inputVal));
-          outputVal.push(
-            noteArr1[(i + addingFactor) % noteArr1.length] +
-              inputSlice(inputVal)
-          );
-        }
-      }
-    }
-
-    if (valid) {
-      // For output
-      let nOutput = document.getElementById("newOutput");
-      let oOutput = document.getElementById("oldOutput");
-
-      let newOutputString = "";
-      let oldOldputString = "";
-
-      for (let i = 0; i < inputArr.length; i++) {
-        oldOldputString += inputArr[i] + "   ";
-        newOutputString += outputVal[i] + "   ";
-      }
-
-      oOutput.innerText = oldOldputString;
-
-      nOutput.innerText = newOutputString;
-
-      document.getElementById("oldOutputCard").style.visibility = "visible";
-      document.getElementById("newOutputCard").style.visibility = "visible";
-    } else {
-      showWarning();
-    }
+    //Displays the output
+    displayOutput(inputArr, outputArr);
   } else {
-    if (newK === -1) {
+    if (!isNewKValid) {
       newKID.style.border = "red solid 2px";
       if (newKID.value === "") {
         showKeyWarning();
@@ -113,7 +67,7 @@ function transposeProcess(e) {
         showWarning();
       }
     }
-    if (prevK === -1) {
+    if (!isPrevKValid) {
       prevKID.style.border = "red solid 2px";
       if (prevKID.value === "") {
         showKeyWarning();
@@ -122,6 +76,72 @@ function transposeProcess(e) {
       }
     }
   }
+}
+
+// Checks each input node and processes the input values
+function processInput(inputArr, outputArr, addingFactor) {
+  for (let j = 1; j <= inputLength; j++) {
+    let inputID = document.getElementById(`user-input${j}`);
+    let inputVal = inputID.value;
+
+    if (inputVal != "") {
+      inputVal = inputVal.trim();
+      inputVal = capitalizedFirstLetter(inputVal);
+
+      if (!validateInput(inputVal)) {
+        inputID.style.border = "none";
+        inputID.style.border = "red solid 2px";
+        showWarning();
+      } else {
+        inputID.style.border = "none";
+        inputID.style.border = "green solid 1px";
+
+        storeTransposeResultsInArrays(
+          inputArr,
+          outputArr,
+          inputVal,
+          addingFactor
+        );
+      }
+    }
+  }
+}
+
+//adds the values to input array and output array
+function storeTransposeResultsInArrays(
+  inputArr,
+  outputArr,
+  inputVal,
+  addingFactor
+) {
+  inputArr.push(inputVal);
+  // transpoing and adding the values to the array outputArr
+  let i = noteArr1.indexOf(filterInput(inputVal));
+  outputArr.push(
+    noteArr1[(i + addingFactor) % noteArr1.length] + inputSlice(inputVal)
+  );
+}
+
+// displays the output
+function displayOutput(inputArr, outputArr) {
+  // For output
+  let nOutput = document.getElementById("newOutput");
+  let oOutput = document.getElementById("oldOutput");
+
+  let newOutputString = "";
+  let oldOldputString = "";
+
+  for (let i = 0; i < inputArr.length; i++) {
+    oldOldputString += inputArr[i] + "   ";
+    newOutputString += outputArr[i] + "   ";
+  }
+
+  oOutput.innerText = oldOldputString;
+
+  nOutput.innerText = newOutputString;
+
+  document.getElementById("oldOutputCard").style.visibility = "visible";
+  document.getElementById("newOutputCard").style.visibility = "visible";
 }
 
 // pre: accepts a string as parameter
@@ -149,8 +169,11 @@ function showKeyWarning() {
 
 // filters the input to get only the values that match with values of noteArr1
 function filterInput(input) {
-  if (input.charAt(1) === "b" || input.charAt(1) === "#") {
+  if (input.charAt(1) === "#") {
     return input.substring(0, 2);
+  } else if (input.charAt(1) === "b") {
+    let index = noteArr1.indexOf(input.charAt(0)) - 1;
+    return noteArr1[index];
   } else {
     return input.charAt(0);
   }
@@ -170,9 +193,8 @@ function validateInput(input) {
   let isValid = false;
 
   let firstHalf = filterInput(input);
-  let secondHalf = inputSlice(input);
 
-  console.log(inputSlice("d").length);
+  let secondHalf = inputSlice(input);
 
   if (noteArr1.indexOf(firstHalf) != -1) {
     if (secondHalf.length > 0) {
